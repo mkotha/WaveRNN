9cd1770a24dbdc43ceaad1968053dae8dfd3f72f
m@kotha.net <m@kotha.net>
2018-09-16 13:11:23 +0900

    move upsampler to its own module

--- models/wavernn1.py
+++ models/wavernn1.py
@@ -12,48 +12,8 @@ import sys
 import time
 import apex
 from layers.wavernn import WaveRNN
+from layers.upsample import UpsampleNetwork
 import utils.env as env
-
-
-bits = 16
-
-seq_len = hop_length * 5
-
-
-# In[10]:
-
-class Stretch2d(nn.Module) :
-    def __init__(self, x_scale, y_scale) :
-        super().__init__()
-        self.x_scale = x_scale
-        self.y_scale = y_scale
-
-    def forward(self, x) :
-        b, c, h, w = x.size()
-        x = x.unsqueeze(-1).unsqueeze(3)
-        x = x.repeat(1, 1, 1, self.y_scale, 1, self.x_scale)
-        return x.view(b, c, h * self.y_scale, w * self.x_scale)
-
-class UpsampleNetwork(nn.Module) :
-    def __init__(self, upsample_scales, pad):
-        super().__init__()
-        total_scale = np.cumproduct(upsample_scales)[-1]
-        self.indent = pad * total_scale
-        self.up_layers = nn.ModuleList()
-        for scale in upsample_scales :
-            k_size = (1, scale * 2 + 1)
-            padding = (0, scale)
-            stretch = Stretch2d(scale, 1)
-            conv = nn.Conv2d(1, 1, kernel_size=k_size, padding=padding, bias=False)
-            conv.weight.data.fill_(1. / k_size[1])
-            self.up_layers.append(stretch)
-            self.up_layers.append(conv)
-
-    def forward(self, m) :
-        m = m.unsqueeze(1)
-        for f in self.up_layers : m = f(m)
-        m = m.squeeze(1)[:, :, self.indent:-self.indent]
-        return m.transpose(1, 2)
 
 class Model(nn.Module) :
     def __init__(self, rnn_dims, fc_dims, pad, upsample_factors,
