1cc2f555bab347a80cb3651b652524042794f112
m@kotha.net <m@kotha.net>
2018-10-25 00:33:55 +0900

    (vq.37.decoder_only_cp)

--- wavernn.py
+++ wavernn.py
@@ -44,7 +44,7 @@ else:
 
 seq_len = hop_length * 5
 
-model_name = 'vq.37.decoder_only'
+model_name = 'vq.37.decoder_only_cp'
 
 if platform.node().endswith('.ec2') or platform.node().startswith('ip-'): # Running on EC2
     DATA_PATH = '/home/ubuntu/dataset/lj2'
