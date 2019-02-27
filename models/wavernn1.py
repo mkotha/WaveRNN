e46f5f9d3b0d0a86982190407c1d5dd3d40ff99c
m@kotha.net <m@kotha.net>
2019-02-27 20:27:38 +0900

    wavernn1: revert back from overtone to wavernn

--- models/wavernn1.py
+++ models/wavernn1.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 from utils.dsp import *
 import sys
 import time
-from layers.overtone import Overtone
+from layers.wavernn import WaveRNN
 from layers.upsample import UpsampleNetwork
 import utils.env as env
 import utils.logger as logger
@@ -18,16 +18,21 @@ class Model(nn.Module) :
     def __init__(self, rnn_dims, fc_dims, pad, upsample_factors, feat_dims):
         super().__init__()
         self.n_classes = 256
-        self.overtone = Overtone(rnn_dims, fc_dims, feat_dims)
+        self.upsample = UpsampleNetwork(feat_dims, upsample_factors)
+        self.wavernn = WaveRNN(rnn_dims, fc_dims, feat_dims, 0)
         self.num_params()
 
     def forward(self, x, mels) :
         #logger.log(f'x: {x.size()} mels: {mels.size()}')
+        cond = self.upsample(mels)
         #logger.log(f'cond: {cond.size()}')
-        return self.overtone(x, mels.transpose(1, 2))
+        return self.wavernn(x, cond.transpose(1, 2), None, None, None)
 
     def after_update(self):
-        self.overtone.after_update()
+        self.wavernn.after_update()
+
+    def preview_upsampling(self, mels) :
+        return self.upsample(mels)
 
     def forward_generate(self, mels, deterministic=False, use_half=False, verbose=False):
         n = mels.size(0)
@@ -35,7 +40,8 @@ class Model(nn.Module) :
             mels = mels.half()
         self.eval()
         with torch.no_grad() :
-            output = self.overtone.generate(mels.transpose(1, 2), use_half=use_half, verbose=verbose)
+            cond = self.upsample(mels)
+            output = self.wavernn.generate(cond.transpose(1, 2), None, None, None, use_half=use_half, verbose=verbose)
         self.train()
         return output
 
@@ -47,7 +53,7 @@ class Model(nn.Module) :
     def load_state_dict(self, dict):
         return super().load_state_dict(upgrade_state_dict(dict))
 
-    def do_train(self, paths, dataset, optimiser, epochs, batch_size, step, lr=1e-4, valid_ids=[], use_half=False):
+    def do_train(self, paths, dataset, optimiser, epochs, batch_size, step, lr=1e-4, valid_index=[], use_half=False):
         if use_half:
             import apex
             optimiser = apex.fp16_utils.FP16_Optimizer(optimiser, dynamic_loss_scale=True)
@@ -55,16 +61,11 @@ class Model(nn.Module) :
         criterion = nn.NLLLoss().cuda()
         k = 0
         saved_k = 0
-        pad_left = self.overtone.pad()
-        logger.log(f'pad_left = {pad_left}')
 
         for e in range(epochs) :
 
-            trn_loader = DataLoader(
-                    dataset,
-                    collate_fn=lambda b: env.collate(self.overtone.pad(), 16, 1, b),
-                    batch_size=batch_size,
-                    num_workers=2, shuffle=True, pin_memory=True)
+            trn_loader = DataLoader(dataset, collate_fn=lambda batch: env.collate(0, 16, 0, batch), batch_size=batch_size,
+                                    num_workers=2, shuffle=True, pin_memory=True)
 
             start = time.time()
             running_loss_c = 0.
@@ -75,6 +76,7 @@ class Model(nn.Module) :
             for i, (mels, coarse, fine, coarse_f, fine_f) in enumerate(trn_loader) :
 
                 mels, coarse, fine, coarse_f, fine_f = mels.cuda(), coarse.cuda(), fine.cuda(), coarse_f.cuda(), fine_f.cuda()
+                coarse, fine, coarse_f, fine_f = [t[:, hop_length:1-hop_length] for t in [coarse, fine, coarse_f, fine_f]]
                 if use_half:
                     mels = mels.half()
                     coarse_f = coarse_f.half()
@@ -82,9 +84,9 @@ class Model(nn.Module) :
 
                 x = torch.cat([coarse_f[:, :-1].unsqueeze(-1), fine_f[:, :-1].unsqueeze(-1), coarse_f[:, 1:].unsqueeze(-1)], dim=2)
 
-                p_c, p_f = self(x, mels)
-                loss_c = criterion(p_c.transpose(1, 2).float(), coarse[:, pad_left+1:])
-                loss_f = criterion(p_f.transpose(1, 2).float(), fine[:, pad_left+1:])
+                p_c, p_f, _h_n = self(x, mels)
+                loss_c = criterion(p_c.transpose(1, 2).float(), coarse[:, 1:])
+                loss_f = criterion(p_f.transpose(1, 2).float(), fine[:, 1:])
                 loss = loss_c + loss_f
 
                 optimiser.zero_grad()
@@ -109,21 +111,21 @@ class Model(nn.Module) :
             torch.save(self.state_dict(), paths.model_path())
             np.save(paths.step_path(), step)
             logger.log_current_status()
-            logger.log(f' <saved>; w[0][0] = {self.overtone.wavernn.gru.weight_ih_l0[0][0]}')
+            logger.log(f' <saved>; w[0][0] = {self.wavernn.gru.weight_ih_l0[0][0]}')
             if k > saved_k + 50:
                 torch.save(self.state_dict(), paths.model_hist_path(step))
                 saved_k = k
-                self.do_generate(paths, step, dataset.path, valid_ids, use_half=use_half)
+                self.do_generate(paths, step, dataset.path, valid_index, use_half=use_half)
 
-    def do_generate(self, paths, step, data_path, test_ids, deterministic=False, use_half=False, verbose=False):
+    def do_generate(self, paths, step, data_path, test_index, deterministic=False, use_half=False, verbose=False):
         k = step // 1000
-        test_mels = [np.load(f'{data_path}/mel/{id}.npy') for id in test_ids]
+        test_mels = [np.load(f'{data_path}/mel/{id}.npy') for id in test_index]
         maxlen = max([x.shape[1] for x in test_mels])
         aligned = [torch.cat([torch.FloatTensor(x), torch.zeros(80, maxlen-x.shape[1]+1)], dim=1) for x in test_mels]
         out = self.forward_generate(torch.stack(aligned).cuda(), deterministic, use_half=use_half, verbose=verbose)
 
         os.makedirs(paths.gen_path(), exist_ok=True)
-        for i, id in enumerate(test_ids):
+        for i, id in enumerate(test_index):
             gt = np.load(f'{data_path}/quant/{id}.npy')
             gt = (gt.astype(np.float32) + 0.5) / (2**15 - 0.5)
             librosa.output.write_wav(f'{paths.gen_path()}/{k}k_steps_{i}_target.wav', gt, sr=sample_rate)
@@ -139,16 +141,16 @@ def upgrade_state_dict(state_dict):
     return out_dict
 
 UPGRADE_KEY = {
-        "rnn.weight_ih_l0": "overtone.gru.weight_ih_l0",
-        "rnn.weight_hh_l0": "overtone.gru.weight_hh_l0",
-        "rnn.bias_ih_l0": "overtone.gru.bias_ih_l0",
-        "rnn.bias_hh_l0": "overtone.gru.bias_hh_l0",
-        "fc1.weight": "overtone.fc1.weight",
-        "fc1.bias": "overtone.fc1.bias",
-        "fc2.weight": "overtone.fc2.weight",
-        "fc2.bias": "overtone.fc2.bias",
-        "fc3.weight": "overtone.fc3.weight",
-        "fc3.bias": "overtone.fc3.bias",
-        "fc4.weight": "overtone.fc4.weight",
-        "fc4.bias": "overtone.fc4.bias",
+        "rnn.weight_ih_l0": "wavernn.gru.weight_ih_l0",
+        "rnn.weight_hh_l0": "wavernn.gru.weight_hh_l0",
+        "rnn.bias_ih_l0": "wavernn.gru.bias_ih_l0",
+        "rnn.bias_hh_l0": "wavernn.gru.bias_hh_l0",
+        "fc1.weight": "wavernn.fc1.weight",
+        "fc1.bias": "wavernn.fc1.bias",
+        "fc2.weight": "wavernn.fc2.weight",
+        "fc2.bias": "wavernn.fc2.bias",
+        "fc3.weight": "wavernn.fc3.weight",
+        "fc3.bias": "wavernn.fc3.bias",
+        "fc4.weight": "wavernn.fc4.weight",
+        "fc4.bias": "wavernn.fc4.bias",
         }
